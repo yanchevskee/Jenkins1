4a89efabe737a6355a928f43432ff1cf8cafb7fe
yanchevskee <165979115+yanchevskee@users.noreply.github.com>
2026-02-24 21:39:13 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        System.out.println("About to crash...");
+        String s = null;
+        System.out.println(s.length()); // NullPointerException at runtime
     }
 }
